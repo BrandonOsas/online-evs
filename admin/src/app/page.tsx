785189047f45d2f1a2ff7986791a1a2f74d012bb
BrandonOsas <brandon.egonmwan@gmail.com>
2023-09-08 12:30:10 +0100

    modified the admin entry page

--- admin/src/app/page.tsx
+++ admin/src/app/page.tsx
@@ -2,24 +2,29 @@
 import { User, onAuthStateChanged } from "firebase/auth";
 import Tabs from "./home/Tabs";
 import { auth } from "../../firebase.config";
-import { Fragment, useState } from "react";
+import { Fragment, useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import TopBar from "./home/TopBar";
 
 export default function Home() {
-  const [user, setUser] = useState<User>();
+  const [user, setUser] = useState<User | null>(null);
   const router = useRouter();
 
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      setUser(user);
-      console.log(user);
-    } else {
-      // User is signed out
-      // redirect("/auth");
-      return router.push("/auth");
-    }
-  });
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+        console.log(user);
+      } else {
+        // User is signed out
+        // redirect("/auth");
+        router.push("/auth");
+      }
+    });
+
+    // Cleanup the subscription when the component unmounts
+    return () => unsubscribe();
+  }, [router]);
 
   if (user) {
     return (
@@ -29,4 +34,7 @@ export default function Home() {
       </Fragment>
     );
   }
+
+  // You can return a loading indicator or anything else while waiting for authentication
+  return <div>Loading...</div>;
 }
